e49705948fe339064d94e118918ca5d1019cf26a
Aldo <xlight97@gmail.com>
2023-04-26 19:20:41 +0800

    Section - React Asynchronous Data [Finished]

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,85 @@
 import React from 'react';
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const getAsyncStories = () =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000);
+  });
+
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then((result) => {
+      setStories(result.data.stories);
+    });
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleRemoveStory = (item) => {
+    const newStory = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStory);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <>
+      <h1>My Hacker Stories</h1>
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+
+      <hr />
+
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+    </>
+  );
+};
+
 const InputWithLabel = ({
   id,
   value,
@@ -9,7 +89,6 @@ const InputWithLabel = ({
   children,
 }) => {
   const inputRef = React.useRef();
-  console.log(inputRef);
 
   React.useEffect(() => {
     if (isFocused && inputRef.current) {
@@ -58,74 +137,5 @@ const Item = ({ item, onRemoveItem }) => (
     <button onClick={() => onRemoveItem(item)}>Dismiss</button>
   </li>
 );
-
-const useStorageState = (key, initialState) => {
-  const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value, key]);
-
-  return [value, setValue];
-};
-
-const App = () => {
-  const initialStories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
-  const [stories, setStories] = React.useState(initialStories);
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleRemoveStory = (item) => {
-    const newStory = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-    setStories(newStory);
-  };
-
-  const searchedStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  return (
-    <>
-      <h1>My Hacker Stories</h1>
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearch}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      <hr />
-
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
-    </>
-  );
-};
 
 export default App;
